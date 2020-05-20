8b155eeebc61593ee7d555e89e3d26ca8235b84d
David Yu <david.yu@yale.edu>
2020-05-20 18:21:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,9 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   data_t = {}
   data.to_a.each do |attribute, value_list|
+    puts value_list
     value_list.to_a.each do |value, name_list|
+      puts name_list
       name_list.each do |name|
         data_t[name] = {} if data_t[name] == nil
         data_t[name][attribute.to_sym] = [] if data_t[name][attribute.to_sym] == nil
